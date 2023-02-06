d1a16567f0acfa6ca29838e21ff3ff921592c767
Yoann L'hostis <yoannlhostis@MacBook-Air-de-Yoann.local>
2023-02-06 16:22:52 +0100

    feat: add name validation in Form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.section`
   display: flex;
   flex-direction: column;
-  height: 120px;
+  height: 160px;
   margin: auto;
 `
 
@@ -24,33 +24,58 @@ const Button = styled.button`
   margin-left: 5px;
 `
 
+const ErrorMessage = styled.p`
+  text-align: center;
+  margin-top: 5px;
+  font-size: 0.8em;
+  color: red;
+`
+
 interface FormProps {
   listeningClickForm: () => void
 }
 
 export const Form = ({ listeningClickForm }: FormProps) => {
   const [name, setName] = useState<string>('')
+  const [error, setError] = useState<string>()
   function handleChangeInput(e: React.FormEvent<HTMLInputElement>) {
     setName(e.currentTarget.value)
   }
+  function isValidName(name: string) {
+    if (name.length < 3) {
+      setError('Ce nom est trop court')
+      return false
+    }
+    if (name.length >= 3) {
+      setError('')
+      return true
+    }
+  }
+  useEffect(() => {
+    if (name.length >= 3) setError('')
+  }, [name])
   async function handleClickButton(e: React.MouseEvent) {
     e.preventDefault()
-    try {
-      const req = fetch('http://localhost:3000/api/names', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name }),
-      })
-        .then((response) => response.json())
-        .then((response) => console.log(JSON.stringify(response)))
-      setName('')
-    } catch (err) {
-      console.log(err)
+    if (!isValidName(name)) {
+      return
+    } else {
+      try {
+        const req = fetch('http://localhost:3000/api/names', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name }),
+        })
+          .then((response) => response.json())
+          .then((response) => console.log(JSON.stringify(response)))
+        setName('')
+      } catch (err) {
+        console.log(err)
+      }
+      listeningClickForm()
     }
-    listeningClickForm()
   }
   return (
     <Container>
@@ -63,6 +88,7 @@ export const Form = ({ listeningClickForm }: FormProps) => {
           onChange={(e) => handleChangeInput(e)}
         />
         <Button onClick={(e) => handleClickButton(e)}>Envoyer</Button>
+        {error ? <ErrorMessage>{error}</ErrorMessage> : null}
       </WrapperForm>
     </Container>
   )
